153631ab9b043d32a7784dc60c7a1fa54383ec25
Prabhat Telidevara <prabhat.tp@gmail.com>
2018-01-28 15:53:40 -0800

    A simple Neural network for 3 epochs

---
+++ Traffic_Sign_Classifier.py
@@ -0,0 +1,47 @@
+# Load pickled data
+import pickle
+import numpy as np
+import tensorflow as tf
+#tf.python.control_flow_ops = tf
+
+with open('/home/prabhat/PycharmProjects/Keras-Intro/small_traffic_set/small_train_traffic.p', mode='rb') as f:
+    data = pickle.load(f)
+
+X_train, y_train = data['features'], data['labels']
+
+print ("Shape of training dataset: ", np.shape(X_train), np.shape(y_train))
+
+
+# Initial Setup for Keras
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Flatten
+
+batch_size = 128
+num_classes = 5
+epochs = 20
+
+# TODO: Build the Fully Connected Neural Network in Keras Here
+model = Sequential()
+model.add(Flatten(input_shape = (32,32,3)))
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dense(5))
+model.add(Activation('softmax'))
+
+
+# preprocess data
+X_normalized = np.array(X_train / 255.0 - 0.5 )
+
+print ("Shape of normalized training dataset: ", np.shape(X_normalized))
+
+from sklearn.preprocessing import LabelBinarizer
+label_binarizer = LabelBinarizer()
+y_one_hot = label_binarizer.fit_transform(y_train)
+
+print ("Shape of lables: ", np.shape(y_one_hot))
+print ("datapoint: ", y_one_hot[0])
+
+
+model.compile('adam', 'categorical_crossentropy', ['accuracy'])
+
+history = model.fit(X_normalized, y_one_hot, nb_epoch=3, validation_split=0.2)
