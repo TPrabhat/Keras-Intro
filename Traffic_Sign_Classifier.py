3e47fba91ae24c47330b8c5d5f961faf02b0662d
Prabhat Telidevara <prabhat.tp@gmail.com>
2018-01-28 17:04:11 -0800

    Added maxpolling and dropout layer

--- Traffic_Sign_Classifier.py
+++ Traffic_Sign_Classifier.py
@@ -14,7 +14,9 @@ print ("Shape of training dataset: ", np.shape(X_train), np.shape(y_train))
 
 # Initial Setup for Keras
 from keras.models import Sequential
-from keras.layers.core import Dense, Activation, Flatten
+from keras.layers.core import Dense, Activation, Flatten, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
 
 batch_size = 128
 num_classes = 5
@@ -22,7 +24,9 @@ epochs = 20
 
 # TODO: Build the Fully Connected Neural Network in Keras Here
 model = Sequential()
-model.add(Convolution2D(32, 3, 3, input_shape= (32, 32, 3)))
+model.add(Convolution2D(32, (3, 3), input_shape= (32, 32, 3)))
+model.add(MaxPooling2D(pool_size = (2,2)))
+model.add(Dropout(0.5))
 model.add(Activation('relu'))
 model.add(Flatten(input_shape = (32,32,3)))
 model.add(Dense(128))
@@ -46,4 +50,4 @@ print ("datapoint: ", y_one_hot[0])
 
 model.compile('adam', 'categorical_crossentropy', ['accuracy'])
 
-history = model.fit(X_normalized, y_one_hot, nb_epoch=3, validation_split=0.2)
+history = model.fit(X_normalized, y_one_hot, epochs=3, validation_split=0.2)
